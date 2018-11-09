5af552ecc65d6c329c67227674741aa69809db77
Bartusp007 <41994395+Bartusp007@users.noreply.github.com>
2018-11-09 11:18:09 +0100

    Add files via upload

---
+++ CalendarTests/ReserveTableTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Tests
+{
+    [TestClass()]
+    public class ReserveTableTests
+    {
+        [TestMethod()]
+        public void ReserveTableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReserveTableTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckIfTableIsAvailableAndReturnWhichTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReturnResultTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
