7e77f8ebc0b2218697fd1c679389ca8ae9420c0a
Bartusp007 <41994395+Bartusp007@users.noreply.github.com>
2018-11-09 11:14:44 +0100

    Add files via upload

---
+++ Calendar.Service.Test/ReservationServiceTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+using Calendar.Data;
+using Calendar.Data.Infrastructure;
+using Calendar.Data.Repositories;
+using Calendar.ViewModel;
+using FakeItEasy;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Calendar.Service.Tests
+{
+    [TestClass]
+    public class ReservationServiceTest
+    {
+        
+        [TestCase(8)]
+        [TestCase(1)]
+        public void WhenCheckingPreReservation_ThenReturnTrue(int howmanyPeoples)
+        {
+            var preReservation = new PreReservationViewModel
+            {
+                FromWhatTime=new TimeSpan(12,0,0),
+                ToWhatTime= new TimeSpan(13, 30, 0),
+                HowManyPeoples=howmanyPeoples,
+                ReservationDate=new DateTime(2018,9,19)
+            };
+
+            IServiceReservation service = new ReservationService();
+            var check = service.CheckPreReservation(preReservation);
+            Assert.IsTrue(check);
+        }
+     
+        [TestCase(9)]
+        [TestCase(34)]
+        public void WhenCheckingPreReservation_ThenReturnFalse(int howmanyPeoples)
+        {
+            var preReservation = new PreReservationViewModel
+            {
+                FromWhatTime = new TimeSpan(12, 0, 0),
+                ToWhatTime = new TimeSpan(13, 30, 0),
+                HowManyPeoples = howmanyPeoples,
+                ReservationDate = new DateTime(2018, 9, 19)
+            };
+
+            IServiceReservation service = new ReservationService();
+            var check = service.CheckPreReservation(preReservation);
+            Assert.IsFalse(check);
+        }
+
+        [TestCase(6)]
+        public void WhenCheckingPreReservation_ThenReturnTwotables(int howmanyPeoples)
+        {
+            var preReservation = new PreReservationViewModel
+            {
+                FromWhatTime = new TimeSpan(12, 0, 0),
+                ToWhatTime = new TimeSpan(13, 30, 0),
+                HowManyPeoples = howmanyPeoples,
+                ReservationDate = new DateTime(2018, 9, 19)
+            };
+
+            IServiceReservation service = new ReservationService();
+            var check = service.GetAvailable(preReservation);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
